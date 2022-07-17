9a340af726736d5bdb56995980761139ee53af93
GustavoCarpio69 <luis.gustavo.carpio.eapeco@gmail.com>
2022-07-17 00:54:46 -0500

    Agregamos los modulos a usar

---
+++ pregunta1.py
@@ -0,0 +1,11 @@
+# Agregamos los modulos que se pueden usar en la solucion de la evaluacion
+
+import math as m
+import random as rnd
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
